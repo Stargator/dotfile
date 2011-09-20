a715b0392f8dc18af8c68c3def40baef82e1ad20
Kelsey Judson <kelseyjudson@gmail.com>
2011-09-20 10:17:14 -0700

    More auto customization to gnome-terminal through gconf.

--- install.sh
+++ install.sh
@@ -10,14 +10,23 @@ if [ -d $HOME/.config/sublime-text-2 ]; then
 fi
 
 if [ -x '/usr/bin/gconftool-2' ]; then
-  echo "Changing gnome-terminal colour scheme."
+  echo "Changing gnome-terminal visual theme."
   fg_key='/apps/gnome-terminal/profiles/Default/foreground_color'
   fg_value='#AAAAAAAAAAAA'
   bg_key='/apps/gnome-terminal/profiles/Default/background_color'
-  bg_value='#000000000000'
+  bg_value='#101010101010'
   palette_key='/apps/gnome-terminal/profiles/Default/palette'
   palette_value='#101010101010:#E5E522222222:#A6A6E3E32D2D:#FCFC95951E1E:#C4C48D8DFFFF:#FAFA25257373:#6767D9D9F0F0:#F2F2F2F2F2F2:#101010101010:#E5E522222222:#A6A6E3E32D2D:#FCFC95951E1E:#C4C48D8DFFFF:#FAFA25257373:#6767D9D9F0F0:#F2F2F2F2F2F2'
+  col_key='/apps/gnome-terminal/profiles/Default/default_size_columns'
+  col_value='120'
+  row_key='/apps/gnome-terminal/profiles/Default/default_size_rows'
+  row_value='32'
+  menu_key='/apps/gnome-terminal/profiles/Default/default_show_menubar'
+  menu_value='False'
   gconftool-2 --type string --set $fg_key $fg_value
   gconftool-2 --type string --set $bg_key $bg_value
   gconftool-2 --type string --set $palette_key $palette_value
+  gconftool-2 --type string --set $col_key $col_value
+  gconftool-2 --type string --set $row_key $row_value
+  gconftool-2 --type bool --set $menu_key $menu_value
 fi
