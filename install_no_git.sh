b08b17d688bb27f09c0cd101db3e49a7e3d9d590
Kelsey Judson <kelseyjudson@gmail.com>
2011-09-21 00:10:17 -0700

    Fixed setting gnome-terminal size in scripts.

--- install_no_git.sh
+++ install_no_git.sh
@@ -17,16 +17,15 @@ if [ -x '/usr/bin/gconftool-2' ]; then
   bg_value='#101010101010'
   palette_key='/apps/gnome-terminal/profiles/Default/palette'
   palette_value='#101010101010:#E5E522222222:#A6A6E3E32D2D:#FCFC95951E1E:#C4C48D8DFFFF:#FAFA25257373:#6767D9D9F0F0:#F2F2F2F2F2F2:#101010101010:#E5E522222222:#A6A6E3E32D2D:#FCFC95951E1E:#C4C48D8DFFFF:#FAFA25257373:#6767D9D9F0F0:#F2F2F2F2F2F2'
-  col_key='/apps/gnome-terminal/profiles/Default/default_size_columns'
-  col_value='120'
-  row_key='/apps/gnome-terminal/profiles/Default/default_size_rows'
-  row_value='32'
   menu_key='/apps/gnome-terminal/profiles/Default/default_show_menubar'
   menu_value='False'
   gconftool-2 --type string --set $fg_key $fg_value
   gconftool-2 --type string --set $bg_key $bg_value
   gconftool-2 --type string --set $palette_key $palette_value
-  gconftool-2 --type string --set $col_key $col_value
-  gconftool-2 --type string --set $row_key $row_value
   gconftool-2 --type bool --set $menu_key $menu_value
+  # Set geometry as it doesn't work through gconf.
+  gnomet_desktop='/usr/share/applications/gnome-terminal.desktop'
+  sed -e 's/^Exec=gnome-terminal/Exec=gnome-terminal --geometry=120x32/g' $gnomet_desktop > gnome-terminal.desktop
+  echo "Copying new gnome-terminal.desktop (requires root privileges)."
+  sudo mv gnome-terminal.desktop $gnomet_desktop
 fi
