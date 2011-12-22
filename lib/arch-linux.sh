97c8f564a34bcd87cda9f5d0b67fc72d4bbebdf4
Kelsey Judson <kelseyjudson@gmail.com>
2011-12-23 07:43:56 +1300

    Added recommendation for zathura document viewer.

--- lib/arch-linux.sh
+++ lib/arch-linux.sh
@@ -9,8 +9,8 @@ if [ -n "$(cat /proc/version | grep ARCH)" -a ! -x /usr/bin/packer ]; then
   cd ../ && rm -rf packer
   echo "Suggested AUR/Arch packages (install with packer):"
   echo "  - ttf-ubuntu-font-family ttf-ms-fonts ttf-vista-fonts"
-  echo "  - faenza-icon-theme faience-icon-theme zukitwo-themes"
-  echo "  - dwb google-chrome-dev tmux rxvt-unicode wmii"
+  echo "  - faenza-icon-theme faience-icon-theme gnome-shell-theme-elegance"
+  echo "  - dwb google-chrome-dev tmux rxvt-unicode wmii zathura (pdf)"
   echo "  - cmus deadbeef weechat marlin-bzr gvim"
   echo "  - xcalib (to set icc colour profile)"
   echo "  - htop iftop (server administration)"
