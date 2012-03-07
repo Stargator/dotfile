1e9e9086627a78703a64b78994c325787ea05a61
Kelsey Judson <kelseyjudson@gmail.com>
2012-03-07 23:52:03 +1300

    Global settings filename has changed for Sublime Text - updated in script.

--- lib/optional/sublime-text.sh
+++ lib/optional/sublime-text.sh
@@ -4,10 +4,10 @@
 if [ -d $HOME/.config/sublime-text-2 ]; then
   echo "Copying Sublime Text 2 configuration..."
   sublime_dir=$HOME'/.config/sublime-text-2/Packages'
-  sublime_default=$sublime_dir'/Default/Global.sublime-settings'
+  sublime_default=$sublime_dir'/Default/Preferences.sublime-settings'
   # Enable vintage (vi) mode by taking it out of ignored packages.
-  sed -e 's/\["Vintage"\]/\[\]/g' $sublime_default > /tmp/Global.sublime-settings
-  mv /tmp/Global.sublime-settings $sublime_default
+  sed -e 's/\["Vintage"\]/\[\]/g' $sublime_default > /tmp/Preferences.sublime-settings
+  mv /tmp/Preferences.sublime-settings $sublime_default
   cp lib/resources/sublime/user/* $sublime_dir/User/
   cp lib/resources/sublime/theme/* $sublime_dir'/Color Scheme - Default/'
 fi
